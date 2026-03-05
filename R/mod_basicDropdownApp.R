680946053d686cc719aa1231a06ba2779b3f663d
Dana Kojtal <dana.kojtal@outlook.com>
2026-03-05 14:16:44 +0100

    fixed mix up of App names

--- R/mod_basicDropdownApp.R
+++ R/mod_basicDropdownApp.R
@@ -4,28 +4,38 @@
 #'
 #' @param id,input,output,session Internal parameters for {shiny}.
 #'
-#' @noRd 
+#' @noRd
 #'
-#' @importFrom shiny NS tagList 
+#' @importFrom shiny NS tagList
 mod_basicDropdownApp_ui <- function(id) {
   ns <- NS(id)
   tagList(
- 
+    selectInput(
+      inputId = ns("dropdown_menu"),
+      label = "Choose wisely",
+      choices = list(
+        "Option 1" = "option1",
+        "Option 2" = "option2",
+        "Option 3" = "option3",
+        "Option 4" = "option4"
+      )
+    )
   )
 }
-    
+
+
 #' basicDropdownApp Server Functions
 #'
-#' @noRd 
+#' @noRd
 mod_basicDropdownApp_server <- function(id){
   moduleServer(id, function(input, output, session){
     ns <- session$ns
- 
+
   })
 }
-    
+
 ## To be copied in the UI
 # mod_basicDropdownApp_ui("basicDropdownApp_1")
-    
+
 ## To be copied in the server
 # mod_basicDropdownApp_server("basicDropdownApp_1")
--- R/mod_dataVisualizationApp.R
+++ R/mod_dataVisualizationApp.R
@@ -10,16 +10,7 @@
 mod_dataVisualizationApp_ui <- function(id) {
   ns <- NS(id)
   tagList(
-    selectInput(
-      inputId = ns("dropdown_menu"),
-      label = "Choose wisely",
-      choices = list(
-        "Option 1" = "option1",
-        "Option 2" = "option2",
-        "Option 3" = "option3",
-        "Option 4" = "option4"
-      )
-    )
+
   )
 }
 
