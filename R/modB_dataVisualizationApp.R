65352dadddb2f76805c279dd3208c46d0a7a5f9a
Dana Kojtal <dana.kojtal@outlook.com>
2026-03-06 10:53:35 +0100

    created modul B for dataVisualisationApp

--- R/modB_dataVisualizationApp.R
+++ R/modB_dataVisualizationApp.R
@@ -1,6 +1,6 @@
-# Modul A: Gleiche Modul wie aus App 1, wobei Dropdown-Menü die Optionen iris, cars und palmerpenguins::penguins hat.
+# Modul B: Output eines scatter plots (je nach ausgewählten Datensatz im Menü von Modul A)
 
-#' dataVisualizationApp UI Function
+#' Module B: dataVisualizationApp UI Function
 #'
 #' @description A shiny Module.
 #'
@@ -12,22 +12,98 @@
 modB_dataVisualizationApp_ui <- function(id) {
   ns <- NS(id)
   tagList(
-
+    h3("Scatter Plot Visualization"),
+    plotOutput(ns("scatter_plot"), height = "500px"),
   )
 }
 
-#' dataVisualizationApp Server Functions
-#'
-#' @noRd
-modB_dataVisualizationApp_server <- function(id){
-  moduleServer(id, function(input, output, session){
-    ns <- session$ns
 
+#' Module B: Scatter Plot Server Function
+#'
+#' @param selected_dataset Reactive value from Module A
+#' @noRd
+modB_dataVisualizationApp_server <- function(id, selected_dataset) {
+  moduleServer(id, function(input, output, session) {
+
+    # Get current dataset based on Module A selection
+    current_data <- reactive({req(selected_dataset())
+
+      switch(selected_dataset(),
+             "iris" = iris,
+             "cars" = cars,
+             "penguins" = palmerpenguins::penguins)
+    })
+
+    # Create scatter plot
+    output$scatter_plot <- renderPlot({
+      req(current_data())
+
+      data <- current_data()
+      dataset_name <- selected_dataset()
+
+      if(dataset_name == "iris") {
+        # Iris: Sepal Length vs Sepal Width
+        plot(data$Sepal.Length, data$Sepal.Width,
+             main = "Iris: Sepal Length vs Sepal Width",
+             xlab = "Sepal Length (cm)",
+             ylab = "Sepal Width (cm)",
+             col = as.factor(data$Species),
+             pch = 19, cex = 1.5)
+        legend("topright",
+               legend = levels(as.factor(data$Species)),
+               col = 1:3, pch = 19,
+               title = "Species")
+
+      } else if(dataset_name == "cars") {
+        # Cars: Speed vs Distance
+        plot(data$speed, data$dist,
+             main = "Cars: Speed vs Stopping Distance",
+             xlab = "Speed (mph)",
+             ylab = "Stopping Distance (ft)",
+             col = "blue", pch = 19, cex = 1.5)
+        # Add trend line
+        abline(lm(dist ~ speed, data = data), col = "red", lwd = 2)
+
+      } else if(dataset_name == "penguins") {
+        # Penguins: Bill Length vs Bill Depth
+        # Remove rows with missing values
+        data_clean <- data[complete.cases(data$bill_length_mm, data$bill_depth_mm), ]
+
+        plot(data_clean$bill_length_mm, data_clean$bill_depth_mm,
+             main = "Penguins: Bill Length vs Bill Depth",
+             xlab = "Bill Length (mm)",
+             ylab = "Bill Depth (mm)",
+             col = as.factor(data_clean$species),
+             pch = 19, cex = 1.5)
+        legend("topright",
+               legend = levels(as.factor(data_clean$species)),
+               col = 1:3, pch = 19,
+               title = "Species")
+      }
+    })
+
+    # Plot description
+    output$plot_description <- renderText({
+      descriptions <- list(
+        "iris" = "Relationship between sepal length and width, by species.",
+        "cars" = "Speed vs stopping distance.",
+        "penguins" = "Bill length vs depth measurements, by penguin species."
+      )
+
+      descriptions[[selected_dataset()]]
+    })
   })
 }
 
-## To be copied in the UI
-# mod_dataVisualizationApp_ui("dataVisualizationApp_1")
-
-## To be copied in the server
-# mod_dataVisualizationApp_server("dataVisualizationApp_1")
+# # Simple test app
+# library(golem)
+#
+# ui<- fluidPage(
+#   modB_dataVisualizationApp_ui("test")
+# )
+#
+# server <- function(input, output, session) {
+#   modB_dataVisualizationApp_server("test", selected_dataset = reactive("penguins"))
+# }
+#
+# shinyApp(ui, server)
