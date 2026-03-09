208b1769d6fbbf113e4e8a0d38d3537f91c76082
Dana Kojtal <dana.kojtal@outlook.com>
2026-03-09 07:43:55 +0100

    created Modul B for dataAnalysisApp

--- R/modB_dataAnalysisApp.R
+++ R/modB_dataAnalysisApp.R
@@ -9,17 +9,71 @@
 #' @noRd
 #'
 #' @importFrom shiny NS tagList
-modB_dataAnalysis_ui <- function(id) {
+modB_dataAnalysisApp_ui <- function(id) {
   ns <- NS(id) # namespace System to prevent ID consflicts
-  tagList()
+  tagList(
+    h3("Data Table"),
+    DT::dataTableOutput(ns("data_table"))
+  )
 }
 
 
 #' dataAnalysis Server Functions
 #'
 #' @noRd
-modB_dataAnalysis_server <- function(id){
+modB_dataAnalysisApp_server <- function(id, selected_dataset){
   moduleServer(id, function(input, output, session){
     ns <- session$ns
+
+    # Get current dataset based on Module A selection
+    current_data <- reactive({
+      req(selected_dataset())
+
+      switch(selected_dataset(),
+             "iris" = iris,
+             "cars" = ggplot2::mpg,
+             "penguins" = palmerpenguins::penguins
+      )
+    })
+
+    # Render data table
+    output$data_table <- DT::renderDataTable({
+      req(current_data())
+
+      DT::datatable(
+        current_data(),
+        options = list(
+          pageLength = 15,
+          scrollX = TRUE,
+          scrollY = "400px",
+          searching = TRUE,
+          ordering = TRUE
+        ),
+        caption = paste("Dataset:", selected_dataset()),
+        filter = 'top'  # Add column filters
+      )
+    })
+
   })
 }
+
+
+# # # Test Module B independently
+# library(golem)
+# library(shiny)
+# library(DT)
+#
+#
+# ui <- fluidPage(
+#   titlePanel("Test: Table Module B"),
+#   modB_dataAnalysisApp_ui("test")
+# )
+#
+# server <- function(input, output, session) {
+#   # Simulate Module A output
+#   simulated_selection <- reactive("cars")
+#   modB_dataAnalysisApp_server("test", simulated_selection)
+# }
+#
+# shinyApp(ui, server)
+
